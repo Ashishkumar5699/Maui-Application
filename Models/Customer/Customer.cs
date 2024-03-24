cdb9cddac9c423734fc72ceaf1301e22821a84f0
Vinod Gangwar <vinod.gangwar-ext@agoraplus.com>
2024-03-24 15:08:08 +0530

    Project init

---
+++ Models/Customer/Customer.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace Punjab_Ornaments.Models
+{
+    public class Customer : PunjabOrnaments.Common.Models.Products.Consumer
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int CustmorId { get; set; }
+
+    }
+}
+
---
+++ Models/Products/Silver.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace Punjab_Ornaments.Models.Products
+{
+    public class Silver : PunjabOrnaments.Common.Products.Silver
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+    }
+}
