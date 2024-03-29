149de6b10e4a9e2a8beabd2094de65cba991caa2
Ashihs Kumar <Ashihssarraf7@gmail.com>
2024-03-29 17:50:48 +0530

    project added

---
+++ Application/Presentation/Viewmodels/Common/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using Punjab_Ornaments.Infrastructure.Database;
+using Punjab_Ornaments.Infrastructure.Navigation;
+
+namespace Punjab_Ornaments.Presentation.Viewmodels
+{
+    public class BaseViewModel : ObservableObject
+    {
+        protected readonly IDataService _dataService;
+        protected readonly INavigationService _navigationService;
+        private bool _isBusy;
+
+        public BaseViewModel(IDataService localDataService, INavigationService navigationService)
+        {
+            _dataService = localDataService;
+            _navigationService = navigationService;
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged(nameof(IsBusy));
+            }
+        }
+
+        protected virtual Task InitializeAsync(object obj)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Application/Presentation/Views/Auth/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+using Punjab_Ornaments.Presentation.Viewmodels.Auth;
+using Punjab_Ornaments.Presentation.Viewmodels.Common;
+
+namespace Punjab_Ornaments.Presentation.Views.Auth;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		try
+		{
+			InitializeComponent();
+			BindingContext = ServiceHelper.GetService<LoginPageViewModel>();
+		}
+		catch (Exception ex)
+		{
+
+		}
+	}
+}
